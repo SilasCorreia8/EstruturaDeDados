1983307534760a2c7817386d772d8c801f5c6c7e
Israel Freire <130157902+israel-freire14@users.noreply.github.com>
2025-03-10 21:10:24 -0300

    feat: adicionando submenu aos cases 6 e 4

--- src/main/java/TrabalhoED/InterfaceDoPrograma.java
+++ src/main/java/TrabalhoED/InterfaceDoPrograma.java
@@ -23,22 +23,24 @@ public class InterfaceDoPrograma implements Serializable {
 
 
             System.out.println("""
-             _      ____ _____ ______   ____      ___      ___      ____    ___   ___ ___    ___  _____
-            | |    |    / ___/|      | /    |    |   \\    /  _]    |    \\  /   \\ |   |   |  /  _]/ ___/       
-            | |     |  (   \\_ |      ||  o  |    |    \\  /  [_     |  _  ||     || _   _ | /  [_(   \\_         
-            | |___  |  |\\__  ||_|  |_||     |    |  D  ||    _]    |  |  ||  O  ||  \\_/  ||    _]\\__  |        
-            |     | |  |/  \\ |  |  |  |  _  |    |     ||   [_     |  |  ||     ||   |   ||   [_ /  \\ |         
-            |     | |  |\\    |  |  |  |  |  |    |     ||     |    |  |  ||     ||   |   ||     |\\    |         
-            |_____||____|\\___|  |__|  |__|__|    |_____||_____|    |__|__| \\___/ |___|___||_____| \\___|         
+             
+               ____   ____    ___  ____   ___     ____\s
+             /    | /    |  /  _]|    \\ |   \\   /    |
+            |  o  ||   __| /  [_ |  _  ||    \\ |  o  |
+            |     ||  |  ||    _]|  |  ||  D  ||     |
+            |  _  ||  |_ ||   [_ |  |  ||     ||  _  |
+            |  |  ||     ||     ||  |  ||     ||  |  |
+            |__|__||___,_||_____||__|__||_____||__|__| 
+             
                     
             -------------------------------------------------------------------------------------------                                                                                                        
-            | 1. Adicionar nome                                                                        | 
-            | 2. Deletar nome                                                                          |   
+            | 1. Adicionar pessoa                                                                      | 
+            | 2. Excluir pessoa                                                                        |   
             | 3. Mostrar lista                                                                         | 
-            | 4. Inserir lista de nomes em uma fila                                                    |                                    
+            | 4. Inserir lista de pessoas em uma fila                                                  |                                    
             | 5. Pessoas que possuem o mesmo sobrenome                                                 |         
-            | 6. Inserir nomes de pessoas em uma arvore                                                | 
-            | 7. Salvar lista                                                                          |
+            | 6. Inserir pessoas em uma arvore                                                         | 
+            | 7. Salvar lista em arquivo                                                               |
             | 0. Encerrar programa                                                                     |                           
             |__________________________________________________________________________________________|  
             """);
@@ -54,6 +56,7 @@ public class InterfaceDoPrograma implements Serializable {
             switch (option) {
                 case 1:
                     scan.nextLine();
+                    System.out.println("___________________________________________________________________________________________");
                     System.out.print("Informe o nome da pessoa que deseja adicionar:");
                     String nomeParaAdicionar = scan.nextLine();
 
@@ -66,10 +69,13 @@ public class InterfaceDoPrograma implements Serializable {
                     listaNomes.adicionar(pessoa);
                     System.out.println(listaNomes.toString());
                     System.out.println("A pessoa de nome '" + nomeParaAdicionar + "' foi adicionada a lista com sucesso.");
+                    System.out.println("___________________________________________________________________________________________");
+
                     break;
 
                 case 2:
                     scan.nextLine();
+                    System.out.println("___________________________________________________________________________________________");
                     System.out.println("Digite o nome da pessoa  que deseja apagar da lista:");
                     String nomeParaDeletar = scan.nextLine();
 
@@ -88,59 +94,147 @@ public class InterfaceDoPrograma implements Serializable {
                         System.out.println("Pessoa nao encontrada na lista");
                         break;
                     }
+                    System.out.println("___________________________________________________________________________________________");
 
                 case 3:
-
+                    System.out.println("___________________________________________________________________________________________");
                     System.out.println("Lista atual: \n");
                     listaNomes.mostrarLista();
+                    System.out.println("___________________________________________________________________________________________");
                     break;
 
 
                 case 4:
                     //Criar submenu para este caso
+                    System.out.println("""
+                     _____  ____  _       ____\s
+                    |     ||    || |     /    |
+                    |   __| |  | | |    |  o  |
+                    |  |_   |  | | |___ |     |
+                    |   _]  |  | |     ||  _  |
+                    |  |    |  | |     ||  |  |
+                    |__|   |____||_____||__|__|
+                    __________________________________________________________________________________________
+                    | 1. Adicionar pessoa a fila                                                              |  
+                    | 2. Mostrar fila                                                                         |      
+                    |_________________________________________________________________________________________|
+                    """);
+                    System.out.print("\nEntrar com opcao: ");
+                    option = scan.nextInt();
 
-                    scan.nextLine();
-                    System.out.print("Informe o nome da pessoa que deseja adicionar:");
-                    String nomeParaAdicionar1 = scan.nextLine();
+                    switch (option){
+                        case 1:
+                            scan.nextLine();
+                            System.out.println("___________________________________________________________________________________________");
+                            System.out.print("Informe o nome da pessoa que deseja adicionar:");
+                            String nomeParaAdicionar1 = scan.nextLine();
 
-                    System.out.print("Por favor, informe a sua idade:");
-                    int idade1 = scan.nextInt();
-                    scan.nextLine();
+                            System.out.print("Por favor, informe a sua idade:");
+                            int idade1 = scan.nextInt();
+                            scan.nextLine();
 
-                    Pessoa pessoa1 = new Pessoa(nomeParaAdicionar1, idade1);
-                    fila.adicionar(pessoa1);
-                    fila.mostrarFila();
+                            Pessoa pessoa1 = new Pessoa(nomeParaAdicionar1, idade1);
+                            fila.adicionar(pessoa1);
+                            System.out.println("___________________________________________________________________________________________");
+                            break;
+
+                        case 2:
+                            System.out.println("___________________________________________________________________________________________");
+                            System.out.println("\n");
+                            fila.mostrarFila();
+                            System.out.println("___________________________________________________________________________________________");
+
+                            break;
+
+                        default:
+                            System.out.println("Opcao invalida!");
+                    }
                     break;
 
                 case 5:
                     scan.nextLine();
+                    System.out.println("___________________________________________________________________________________________");
                     System.out.println("Pessoas que possuem o mesmo sobrenome: \n");
                     listaNomes.pilhaPorSegundoNome();
+                    System.out.println("___________________________________________________________________________________________");
                     break;
 
                 case 6:
-                    //Criar submenu para este caso
-                    scan.nextLine();
-                    System.out.print("Inserir pessoas nas arvores: \n");
-                    System.out.print("Informe o nome da pessoa que deseja adicionar:");
-                    String nomeParaAdicionar2 = scan.nextLine();
 
-                    System.out.print("Por favor, informe a sua idade:");
-                    int idade2 = scan.nextInt();
-                    scan.nextLine();
+                    System.out.println("""
+                       ____  ____  __ __   ___   ____     ___\s
+                     /    ||    \\|  |  | /   \\ |    \\   /  _]
+                    |  o  ||  D  )  |  ||     ||  D  ) /  [_\s
+                    |     ||    /|  |  ||  O  ||    / |    _]
+                    |  _  ||    \\|  :  ||     ||    \\ |   [_\s
+                    |  |  ||  .  \\\\   / |     ||  .  \\|     |
+                    |__|__||__|\\_| \\_/   \\___/ |__|\\_||_____|
+                    __________________________________________________________________________________________
+                    | 1. Inserir pessoa em arvore                                                             |  
+                    | 2. Mostrar arvore em ordem                                                              |  
+                    | 3. Mostrar arvore em pre ordem                                                          |      
+                    | 4. Mostrar pessoas nao inseridas                                                        |      
+                    |_________________________________________________________________________________________|
+                    """);
+                    System.out.print("\nEntrar com opcao: ");
+                    option = scan.nextInt();
 
-                    Pessoa pessoa2 = new Pessoa(nomeParaAdicionar2, idade2);
-                    arvore.adicionar(pessoa2);
-                    arvore.mostrarEmOrdem();
-                    arvore.mostrarPreOrdem();
-                    arvore.mostrarNaoInseridas();
+                    switch (option) {
+                       case 1:
+                           scan.nextLine();
+                           System.out.println("___________________________________________________________________________________________");
+                           System.out.print("Inserir pessoas nas arvores: \n");
+                           System.out.print("Informe o nome da pessoa que deseja adicionar:");
+                           String nomeParaAdicionar2 = scan.nextLine();
+
+                           System.out.print("Por favor, informe a sua idade:");
+                           int idade2 = scan.nextInt();
+                           scan.nextLine();
+
+                           Pessoa pessoa2 = new Pessoa(nomeParaAdicionar2, idade2);
+                           arvore.adicionar(pessoa2);
+                           System.out.println("___________________________________________________________________________________________");
+                           break;
+
+                       case 2:
+                           scan.nextLine();
+                           System.out.println("___________________________________________________________________________________________");
+                           System.out.print("Mostrando arvore em ordem: \n");
+                           arvore.mostrarEmOrdem();
+                           System.out.println("___________________________________________________________________________________________");
+                           break;
+
+                       case 3:
+                           scan.nextLine();
+                           System.out.println("___________________________________________________________________________________________");
+                           System.out.print("Mostrando arvore em pre ordem: \n");
+                           arvore.mostrarPreOrdem();
+                           System.out.println("___________________________________________________________________________________________");
+                           break;
+
+                       case 4:
+                           scan.nextLine();
+                           System.out.println("___________________________________________________________________________________________");
+                           System.out.print("Mostrando nao inseridas: \n");
+                           arvore.mostrarNaoInseridas();
+                           System.out.println("___________________________________________________________________________________________");
+                           break;
+
+                       default:
+                           scan.nextLine();
+                           System.out.print("Opcao invalida!! \n");
+                           break;
+                    }
                     break;
+
 
                 case 7:
                     scan.nextLine();
+                    System.out.println("___________________________________________________________________________________________");
                     System.out.print("Salvando lista em arquivo... \n");
                     arquivo.gravarListaEmArquivo(listaNomes);
                     System.out.print("A lista foi salva com sucesso!\n");
+                    System.out.println("___________________________________________________________________________________________");
                     break;
 
                 case 0:
